7c88761280099e537bac373c912376f30c217223
Md Istiaq Ahmed <istiaqahmed121998@gmail.com>
2025-04-18 01:04:35 -0400

    Remove wrongly committed folder

--- inventory-service/src/main/java/com/backend/inventoryservice/infrastructure/persistence/SpringDataJpaInventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.inventoryservice.infrastructure.persistence;
-
-import com.backend.inventoryservice.application.model.InventoryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringDataJpaInventoryRepository extends JpaRepository<InventoryItem, Long> {
-
-}
